52446d495f7c0955615cf0391af6b87f61d568d1
Gyan Lima <gyan.patrick1@gmail.com>
2020-01-29 02:52:51 -0300

    começando um cms

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>Página inicial do Admin</h1>
+@endsection
+
+@section('content')
+    ...
+@endsection
