29d1b35805ff654875fb66d42368d76618cd0563
Gyan Lima <gyan.patrick1@gmail.com>
2020-02-06 02:55:34 -0300

    gerenciando permissoes

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -19,6 +19,7 @@ class UserController extends Controller
 
     public function __construct() {
         $this->middleware('auth');
+        $this->middleware('can:edit-users');
     }
     
     public function index()
